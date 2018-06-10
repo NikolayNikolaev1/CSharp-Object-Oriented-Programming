4ca07f675bbf572e7f1b09fc3227a5e1031c8f49
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-06-10 17:49:45 +0300

    Creating abstract class Bender.

---
+++ CSharp OOP Exam - Avatar/Avatar/Avatar/Entities/Bender.cs
@@ -0,0 +1,14 @@
+﻿namespace Avatar.Entities
+{
+    public abstract class Bender
+    {
+        private string name;
+        private int power;
+
+        public Bender(string name, int power)
+        {
+            this.name = name;
+            this.power = power;
+        }
+    }
+}
