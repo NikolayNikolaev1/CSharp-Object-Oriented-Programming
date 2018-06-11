591bdd060c04b14136c8c8574f06d6069bfb6730
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-06-11 17:11:48 +0300

    Added NationsBuilder class.

---
+++ CSharp OOP Exam - Avatar/Avatar/Avatar/Core/NationsBuilder.cs
@@ -0,0 +1,21 @@
+﻿namespace Avatar.Core
+{
+    using Entities;
+    using System.Collections.Generic;
+
+    public class NationsBuilder
+    {
+        private Dictionary<string, Nation> nations;
+
+        public NationsBuilder()
+        {
+            this.nations = new Dictionary<string, Nation>()
+            {
+                { "Air", new Nation()},
+                { "Fire", new Nation()},
+                { "Water", new Nation()},
+                { "Earth", new Nation()}
+            };
+        }
+    }
+}
