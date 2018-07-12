990d4acec45299fb85e8e3c4ae0e9fd647d038c1
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-12 16:59:24 +0300

    Added all command methods to CarManager.

--- Need For Speed - Exam/NeedForSpeed/Core/CarManager.cs
+++ Need For Speed - Exam/NeedForSpeed/Core/CarManager.cs
@@ -2,5 +2,46 @@
 {
     public class CarManager
     {
+
+        public void Register(int id, string type, string brand, string model, int yearOfProduction,
+            int horsepower, int acceleration, int suspension, int durability)
+        {
+
+        }
+
+        public string Check(int id)
+        {
+            return ToString();
+        }
+
+        public void Open(int id, string type, int length, string route, int prizePool)
+        {
+
+        }
+
+        public void Participate(int carId, int raceId)
+        {
+
+        }
+
+        public string Start(int id)
+        {
+            return ToString();
+        }
+
+        public void Park(int id)
+        {
+
+        }
+
+        public void Unpark(int id)
+        {
+
+        }
+
+        public void Tune(int tuneIndex, string addOn)
+        {
+
+        }
     }
 }
