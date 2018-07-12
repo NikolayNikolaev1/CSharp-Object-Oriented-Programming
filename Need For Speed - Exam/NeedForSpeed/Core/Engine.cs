fb84adc98eea9af8274e956133d4e720eb5cb015
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-07-12 19:19:48 +0300

    Added some methods.

--- Need For Speed - Exam/NeedForSpeed/Core/Engine.cs
+++ Need For Speed - Exam/NeedForSpeed/Core/Engine.cs
@@ -24,36 +24,22 @@
 
         public void ExecuteCommand(string[] cmdArgs)
         {
-            int id = int.Parse(cmdArgs[1]);
-            string type = cmdArgs[2];
+            string command = cmdArgs[0];
 
-            switch (cmdArgs[0])
+            switch (command)
             {
                 case "register":
-                    id = int.Parse(cmdArgs[1]);
-                    type = cmdArgs[2];
-                    string brand = cmdArgs[3];
-                    string model = cmdArgs[4];
-                    int yearOfProduction = int.Parse(cmdArgs[5]);
-                    int horsepower = int.Parse(cmdArgs[6]);
-                    int acceleration = int.Parse(cmdArgs[7]);
-                    int suspension = int.Parse(cmdArgs[8]);
-                    int durability = int.Parse(cmdArgs[9]);
-                    manager.Register(id, type, brand, model, yearOfProduction, horsepower, acceleration, suspension, durability);
+                    this.RegisterCommand(cmdArgs);
                     break;
                 case "check":
-                    id = int.Parse(cmdArgs[1]);
-                    Console.WriteLine(manager.Check(id));
+                    Console.WriteLine(this.CheckCommand(cmdArgs));
                     break;
                 case "open":
-                    id = int.Parse(cmdArgs[1]);
-                    type = cmdArgs[2];
-                    int lenght = int.Parse(cmdArgs[3]);
-                    string route = cmdArgs[4];
-                    int prizePool = int.Parse(cmdArgs[5]);
-                    manager.Open(id, type, lenght, route, prizePool);
+                    this.CheckCommand(cmdArgs);
                     break;
                 case "participate":
+                    int carId = int.Parse(cmdArgs[1]);
+                    int raceId = int.Parse(cmdArgs[2]);
                     break;
                 case "start":
                     break;
@@ -63,7 +49,37 @@
                     break;
                 case "tune":
                     break;
-            }
+            }   
+        }
+
+        private void RegisterCommand(string[] cmdArgs)
+        {
+            int id = int.Parse(cmdArgs[1]);
+            string type = cmdArgs[2];
+            string brand = cmdArgs[3];
+            string model = cmdArgs[4];
+            int yearOfProduction = int.Parse(cmdArgs[5]);
+            int horsepower = int.Parse(cmdArgs[6]);
+            int acceleration = int.Parse(cmdArgs[7]);
+            int suspension = int.Parse(cmdArgs[8]);
+            int durability = int.Parse(cmdArgs[9]);
+            manager.Register(id, type, brand, model, yearOfProduction, horsepower, acceleration, suspension, durability);
+        }
+
+        private string CheckCommand(string[] cmdArgs)
+        {
+            int id = int.Parse(cmdArgs[1]);
+            return manager.Check(id);
+        }
+
+        private void OpenCommand(string[] cmdArgs)
+        {
+            int id = int.Parse(cmdArgs[1]);
+            string type = cmdArgs[2];
+            int lenght = int.Parse(cmdArgs[3]);
+            string route = cmdArgs[4];
+            int prizePool = int.Parse(cmdArgs[5]);
+            manager.Open(id, type, lenght, route, prizePool);
         }
     }
 }
